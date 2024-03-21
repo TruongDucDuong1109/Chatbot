99e778339e2bfab198c61795cffd541a5e000882
UIT20521227 <116501582+TruongDucDuong1109@users.noreply.github.com>
2024-03-21 17:15:42 +0700

    update prompt and model

--- model.py
+++ model.py
@@ -2,20 +2,21 @@ from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.prompts import PromptTemplate
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+
 from langchain_community.llms import CTransformers
 from langchain.chains import RetrievalQA
 import chainlit as cl
 
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
-custom_prompt_template = """Use the following pieces of information to answer the user's question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+custom_prompt_template = """Sử dụng các thông tin sau đây để trả lời câu hỏi của người dùng.
+Nếu không biết câu trả lời, bạn chỉ cần nói rằng bạn không biết, đừng cố bịa ra câu trả lời.
 
 Context: {context}
 Question: {question}
 
-Only return the helpful answer below and nothing else.
-Helpful answer:
+Chỉ trả lại câu trả lời hữu ích bên dưới và không có gì khác.
+Câu trả lời hữu ích:
 """
 
 def set_custom_prompt():
@@ -29,22 +30,25 @@ def set_custom_prompt():
 #Retrieval QA Chain
 
 def retrieval_qa_chain(llm, prompt, db):
+    print("QA Chain created")
     qa_chain = RetrievalQA.from_chain_type(llm=llm,
-                                       chain_type='stuff',
-                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
+                                        chain_type='stuff',
+                                       retriever=db.as_retriever(search_kwargs={'k': 3}),
                                        return_source_documents=True,
                                        chain_type_kwargs={'prompt': prompt}
+                                       
                                        )
+    
     return qa_chain
 
 #Loading the model
 def load_llm():
     # Load the locally downloaded model here
     llm = CTransformers(
-        model = "llama-2-7b-chat.ggmlv3.q8_0.bin",
+        model = "model.Q4_K_M.gguf",
         model_type="llama",
-        max_new_tokens = 512,
-        temperature = 0.5
+        max_new_tokens = 1024,
+        temperature = 0.5,
     )
     return llm
 
@@ -52,7 +56,7 @@ def load_llm():
 def qa_bot():
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
                                        model_kwargs={'device': 'cpu'})
-    db = FAISS.load_local(DB_FAISS_PATH, embeddings)
+    db = FAISS.load_local(DB_FAISS_PATH, embeddings,allow_dangerous_deserialization = True)
     llm = load_llm()
     qa_prompt = set_custom_prompt()
     qa = retrieval_qa_chain(llm, qa_prompt, db)
@@ -71,7 +75,7 @@ async def start():
     chain = qa_bot()
     msg = cl.Message(content="Starting the bot...")
     await msg.send()
-    msg.content = "Hi, Welcome to Medical Bot. What is your query?"
+    msg.content = "Hi, Tôi là bot của bạn , tôi có thể giúp gì cho bạn không?"
     await msg.update()
 
     cl.user_session.set("chain", chain)
@@ -93,3 +97,4 @@ async def main(message: cl.Message):
         answer += "\nNo sources found"
 
     await cl.Message(content=answer).send()
+
